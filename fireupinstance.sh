a51122d04efddcc0d49d3e97f7f581878e1a214e
EC2 Default User <ec2-user@ip-10-112-119-39.ec2.internal>
2011-10-09 18:29:19 -0500

    Initial commit
    A couple scripts I use to automate starting and stoping an
    amazon EC2 instance.
    
    Signed-off-by: Tyler Morten <tyler@mpdigitalsolutions.com>

---
+++ fireupinstance.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PATH=$PATH:/opt/aws/bin
+export PATH
+
+EC2_HOME=/opt/aws/apitools/ec2
+export EC2_HOME
+
+JAVA_HOME=/usr/lib/jvm/jre
+export JAVA_HOME
+
+
+export instance_id=`ec2-run-instances <ami_here> --private-key ~/.ec2/pk.pem --cert ~/.ec2/cert.pem -g <description> -k key -t m1.small | grep INSTANCE | awk '{print $2}'`
+
+echo $instance_id > /tmp/streaming_instance.id
+
+sleep 60 && ec2-associate-address --private-key ~/.ec2/pk.pem --cert ~/.ec2/cert.pem <ip_here> -i $instance_id 
+
---
+++ terminateinstance.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PATH=$PATH:/opt/aws/bin
+export PATH
+
+EC2_HOME=/opt/aws/apitools/ec2
+export EC2_HOME
+
+JAVA_HOME=/usr/lib/jvm/jre
+export JAVA_HOME
+
+instance_id=`cat /tmp/streaming_instance.id`
+echo 'Terminating ' $instance_id
+
+ec2-terminate-instances --private-key ~/.ec2/pk.pem --cert ~/.ec2/cert.pem $instance_id && rm /tmp/streaming_instance.id
+
